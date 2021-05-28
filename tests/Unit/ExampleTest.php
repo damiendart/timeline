44984a8f8c9e34687e86ad16f4fb86845a27205c
Damien Dart <damiendart@pobox.com>
2021-05-28 23:36:31 +0100

    Add initial implementation of some tests.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -12,9 +12,6 @@ use PHPUnit\Framework\TestCase;
  */
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function testExample(): void
     {
         $this->assertTrue(true);
