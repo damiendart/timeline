660c8a125f75cd44a553bd76cd0436395c2f2fec
Damien Dart <damiendart@pobox.com>
2021-07-01 00:14:58 +0100

    Improve timeline appearance.

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -3,24 +3,41 @@
 @section('title', 'Overview')
 
 @section('body')
+    @php
+        $reverse = false;
+    @endphp
     @foreach ($groupedEvents as $year => $months)
-        <h2>{{ $year }}</h2>
-        @foreach ($months as $month => $days)
-            <h3>{{ $month }}</h3>
-            @foreach ($days as $day => $events)
-                <h4>{{ $day }}</h4>
-                @foreach ($events as $event)
-                    <h5>{{ $event->title }}</h5>
-                    <ul>
-                        <li><strong>Event date:</strong> {{ $event->date }}</li>
-                        @if ($event->category !== null)
-                            <li><strong>Event category:</strong> {{ $event->category->title }}</li>
-                            <li><strong>Event category slug:</strong> {{ $event->category->slug }}</li>
-                        @endif
-                    </ul>
-                    <p>{{ $event->description }}</p>
+        <div class="year-section year-section--{{ $year }}" id="{{ $year }}">
+            <div class="year-section__container">
+                @foreach ($months as $month => $days)
+                    <div class="timeline">
+                        <h2 class="timeline__heading">{{ $month }} {{ $year }}</h2>
+                        <div class="timeline__items">
+                            @foreach ($days as $day => $events)
+                                @php
+                                    $reverse = !$reverse;
+                                @endphp
+                                @foreach ($events as $event)
+                                    <div class="timeline__items__item{{ $reverse ? ' timeline__items__item--reverse' : '' }}">
+                                        @php
+                                            /** @var \App\Models\Event $event */
+                                        @endphp
+                                        <div class="event-card event-card--{{ $event->category?->slug ?? 'generic' }}">
+                                            <div class="event-card__header">
+                                                <h3 class="event-card__header__title">{{ $event->title }}</h3>
+                                                <p class="event-card__header__date">{{ $event->date }}</p>
+                                            </div>
+                                            <div class="event-card__body">
+                                                <p>{{ $event->description }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @endforeach
+                        </div>
+                    </div>
                 @endforeach
-            @endforeach
-        @endforeach
+            </div>
+        </div>
     @endforeach
 @endsection
